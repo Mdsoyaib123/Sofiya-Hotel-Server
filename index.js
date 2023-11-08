c2db31cb57ae0143d14f3ec712bb330d01502e87
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-11-08 23:01:47 +0600

    add last file

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const verifyToken = (req,res,next)=>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
 
     const roomsCollection = client.db('hotelDB').collection('rooms')
 
@@ -63,12 +63,14 @@ async function run() {
     app.post('/jwt',async(req,res)=>{
       const user = req.body 
       const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'})
-      res.cookie('token',token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:'none' 
-      })
-      res.send({status:true})
+
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
       
      })
 
