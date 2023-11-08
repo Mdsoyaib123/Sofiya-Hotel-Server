9c8a2af1f74d8f85b2ee5132047b2139d7539b5d
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-11-08 11:21:44 +0600

    review add successfull

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config()
 const cors = require('cors')
@@ -10,7 +11,10 @@ const  cookieParser = require('cookie-parser')
 // meddleware
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true 
+}));
 
 
 
@@ -27,6 +31,20 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken = (req,res,next)=>{
+  const token = req.cookies?.token 
+  if(!token){
+    return res.status(401).send({message:'unAuthorized access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+    if(error){
+      return res.status(401).send({message:'unAuthorized access'})
+    }
+    console.log('decoded token ',decoded);
+    req.user = decoded
+    next()
+  })
+}
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -35,6 +53,20 @@ async function run() {
     const roomsCollection = client.db('hotelDB').collection('rooms')
 
     const bookingCollection = client.db('hotelDB').collection('myBooking')
+
+    // json web token 
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body 
+      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'})
+      res.cookie('token',token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'none' 
+      })
+      res.send({status:true})
+      
+     })
+
 
     app.get('/api/v1/rooms',async(req,res)=>{
       const result= await roomsCollection.find().toArray()
@@ -46,18 +78,57 @@ async function run() {
       const result = await roomsCollection.findOne(query);
       res.send(result)
     })
+
+    app.put('/api/v1/rooms',async(req,res)=>{
+      const ReviewData = req.body
+      // console.log(ReviewData);
+      const query ={room_name:req.query.name}
+      const updateDoc = {
+        $set: {
+         review:ReviewData
+        },
+      };
+     const result = await roomsCollection.updateOne(query,updateDoc)
+     res.send(result)
+    })
+
+
+
+
     app.get('/api/v1/bookingData',async(req,res)=>{
-      const result = await bookingCollection.find().toArray();
+      const result = await bookingCollection.find().toArray()
       res.send(result)
     })
 
-    app.get('/api/v1/bookingData/:id',async(req,res)=>{
+
+
+    app.get('/api/v1/myBooking',verifyToken,async(req,res)=>{
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message:'unAuthorized access'})
+      }
+     
+      
+    
+      if(req.query?.email){
+        const email= req.query.email
+        query={email: email}
+      }
+
+      const cursor = bookingCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+
+    app.get('/api/v1/update/:id',async(req,res)=>{
       const id = req.params.id 
-      const query = {_id:new ObjectId(id)}
+      const query ={_id:new ObjectId(id)}
       const result = await bookingCollection.findOne(query)
       res.send(result)
     })
    
+    
 
 
 
@@ -68,22 +139,46 @@ async function run() {
         res.send(result)
     })
 
-    // app.put('/api/v1/bookingData/:id',async(req,res)=>{
-    //   const date  = req.body 
-    //   // console.log(date.updateDate);
-    //   const id = req.params.id ;
-    //   // console.log(id);
-    //   const filter = {_id:new ObjectId(id)};
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //      date:date.updateDate
-    //     },
-    //   };
-    //   const result = await bookingCollection.updateOne(filter,options,updateDoc)
-    //   res.send(result)
+    app.put('/api/v1/bookingData/:id',async(req,res)=>{
+      const date  = req.body 
+      console.log(date.updateDate);
+      const id = req.params.id ;
+      console.log(id);
+      const filter = {_id:new ObjectId(id)};
+      // const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+         date:date.updateDate
+        },
+      };
+      const result = await bookingCollection.updateOne(filter,updateDoc)
+      res.send(result)
 
-    // })
+    })
+
+    app.delete('/api/v1/bookingData/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id:new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
+
+
+    //   const currentDate = req.body 
+    // //   console.log(currentDate);
+    //   const date = await bookingCollection.findOne(query)
+    //   const BookingDate  = '2023-12-07 18:32:45'
+    //   console.log(BookingDate);
+    //   const oneDayInMillis = 24 * 60 * 60 * 1000;
+    //   const bookingTimestamp = BookingDate.valueOf();
+    //   // console.log(bookingTimestamp);
+    //   const cancellationTimestamp = currentDate.valueOf();
+    //   // console.log(cancellationTimestamp);
+    //   if (bookingTimestamp - cancellationTimestamp >= oneDayInMillis) {
+    //     console.log('Booking can not be cancel');
+    //   } else {
+    //     console.log('You can cancel the booking.'); 
+    //   }
+    })
 
 
     // Send a ping to confirm a successful connection
