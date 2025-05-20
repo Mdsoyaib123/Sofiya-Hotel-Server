7d8bff9638f2c6e0e30cb5581265f60c9fd9e40a
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-05-20 20:42:59 +0600

    update

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-import { App } from './app.js';
 import Config from './src/app/Config/index.js';
+import { App } from './app.js';
 let server;
 
 async function main() {
   try {
     await mongoose.connect(Config.db_url);
 
-    server = App.listen(5000, () => {
-      console.log(`server running on port 5000`);
+    server = App.listen(process.env.PORT, () => {
+      console.log(`server running on port ${process.env.PORT}`);
     });
   } catch (error) {
     console.error(error);
